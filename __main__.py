a7fb304fd13d4bf34594708401a383a2ed39fb9f
Matt Allan <matt@destructure.co>
2023-08-16 20:07:00 -0400

    port init to use more stdlib utils

--- __main__.py
+++ __main__.py
@@ -1,27 +1,15 @@
 import argparse
+import ipaddress
+import json
 import os
 import shutil
 import sys
 import subprocess
+from typing import TypedDict
+import wg
 from string import Template
-
-netdev_template = Template("""[NetDev]
-Name=wg${id}
-Kind=wireguard
-Description=WireGuard tunnel wg${id}
-
-[WireGuard]
-ListenPort=5182${id}
-PrivateKeyFile=/etc/systemd/network/wg${id}_private.key
-""")
-
-network_template = Template("""[Match]
-Name=wg${id}
-
-[Network]
-Address=10.0.0.${id}/24
-Address=fdc9:281f:04d7:9ee9::${id}/64
-""")
+from pathlib import Path
+from configparser import ConfigParser
                             
 # TODO: finish this...
 peer_template= Template("""[WireGuardPeer]
@@ -31,51 +19,84 @@ AllowedIPs=10.0.0.3/32
 AllowedIPs=fdc9:281f:04d7:9ee9::3/128
 """)
                             
-def key_opener(path, flags):
-    """An opener that sets the necessary file mode for wireguard keys"""
+def gr_opener(path, flags):
+    """An opener that sets the file mode to owner=rwx, group=r, other="""
     return os.open(path, flags, mode=0o0640)
 
-def init_network() -> None:
+class InitOptions(TypedDict):
+    conf_dir: Path
+    ifname: str
+    port: int
+    ip_net: ipaddress.IPv4Network | ipaddress.IPv6Network
+    force: bool
+
+def init_network(args: InitOptions) -> None:
     """Initialize a new wireguard network"""
-    if os.getuid() != 0:
-        raise Exception("Must be root")
 
-    # TODO: pass this in from an option
-    id = 0
+    conf_dir: Path = args["directory"]
+    ifname: str = args["interface"]
+    # TODO: find next free port >= 51280 as default if not provided
+    port: int = args["port"]
+    ip_net: ipaddress.IPv4Network | ipaddress.IPv6Network = args["network"]
+    force: bool = args["force"]
 
-    # TODO: check if it already exists
+    if not ip_net.is_private:
+        raise Exception(f"Network CIDR {ip_net} is not allocated for private networks (see RFC 1918 / RFC 4193)")
 
-    print("Creating wireguard network wg{0}...".format(id))
+    if not os.access(conf_dir, os.W_OK):
+        raise Exception(f"The directory {conf_dir} is not writeable (try sudo?)") 
 
-    with open("/etc/systemd/network/99-wg{0}.netdev".format(id), "w") as fh:
-        fh.write(netdev_template.substitute({"id": id}))
-        print(" [+] Created virtual network device {0}".format(fh.name))
+    # Reload first to catch any new config files
+    subprocess.run(["networkctl", "reload"], check=True)
 
-    with open("/etc/systemd/network/99-wg{0}.network".format(id), "w") as fh:
-        fh.write(network_template.substitute({"id": id}))
-        print(" [+] Created network interface {0}".format(fh.name))
+    links = json.loads(subprocess.run(["networkctl", "list", "--json", "short"], capture_output=True).stdout)
 
-    dropin_dir = "/etc/systemd/network/99-wg{0}.netdev.d".format(id)
-    os.makedirs(dropin_dir, mode=0o0750, exist_ok=True)
-    print(" [+] Created drop-in directory {0}".format(dropin_dir))
+    for iface in links["Interfaces"]:
+        if iface["Name"] == ifname and not force:
+            raise Exception(f"The interface {ifname} already exists (use --force to overwrite)")
 
-    private_key = subprocess.run(["wg", "genkey"], check=True, capture_output=True).stdout
+    print(f"Creating wireguard network {ifname}...", file=sys.stderr)
 
-    with open("/etc/systemd/network/wg{0}_private.key".format(id), "w", opener=key_opener) as fh:
-        fh.write(private_key.decode())
+    private_key = wg.genkey()
+
+    netdev = ConfigParser()
+    netdev.optionxform = lambda option: option
+    netdev["NetDev"] = {
+        "Name": ifname,
+        "Kind": "wireguard",
+        "Description": f"wireguard tunnel {ifname}",
+    }
+
+    netdev["WireGuard"] = {
+        "ListenPort": port,
+        "PrivateKey": private_key,
+    }
+
+    network = ConfigParser()
+    network.optionxform = lambda option: option
+    network["Match"] = {
+        "Name": ifname,
+    }
+    network["Network"] = {
+        "Address": str(ip_net),
+    }
+
+    # Write netdev
+    with open(conf_dir / f"99-{ifname}.netdev", "w", opener=gr_opener) as fh:
+        netdev.write(fh)
         shutil.chown(fh.name, "root", "systemd-network")
-        print(" [+] Generated private key {0}".format(fh.name))
+        print(f" [+] Created virtual network device {fh.name}", file=sys.stderr)
 
-    public_key = subprocess.run(["wg", "pubkey"], input=private_key, check=True, capture_output=True).stdout
-
-    with open("/etc/systemd/network/wg{0}_public.key".format(id), "w", opener=key_opener) as fh:
-        fh.write(public_key.decode())
+    with open(conf_dir / f"99-{ifname}.network", "w", opener=gr_opener) as fh:
+        network.write(fh)
         shutil.chown(fh.name, "root", "systemd-network")
-        print(" [+] Generated public key {0}".format(fh.name))
-    
+        print(f" [+] Created virtual network interface {fh.name}", file=sys.stderr)
+
     subprocess.run(["networkctl", "reload"])
-    print(" [-] Reloaded systemd-networkd")
+    
+    print(" [-] Reloaded systemd-networkd", file=sys.stderr)
 
+# TODO: rewrite this
 def add_peer() -> None:
     """Add a wireguard peer to the given network"""
 
@@ -93,7 +114,7 @@ def add_peer() -> None:
 
     public_key = subprocess.run(["wg", "pubkey"], input=private_key, check=True, capture_output=True).stdout
 
-    with open(dropin_dir + "/{0}_public.key".format(peer_id), "w", opener=key_opener) as fh:
+    with open(dropin_dir + "/{0}_public.key".format(peer_id), "w", opener=gr_opener) as fh:
         fh.write(public_key.decode())
         shutil.chown(fh.name, "root", "systemd-network")
         print(" [+] Generated public key {0}".format(fh.name))
@@ -108,27 +129,26 @@ def add_peer() -> None:
 
     # TODO: write conf to stdout, which can be qrencoded with a pipe
 
-commands = {
-    "init": init_network,
-    "add": add_peer,
-}
-
 def main() -> int:
     """Invoke the command line application and return the exit code"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "command",
-        choices=commands.keys(),
-        nargs=1,
-    )
+    parser = argparse.ArgumentParser(prog="mkwg")
+    parser.add_argument("-C", "--directory", help="Root configuration directory", type=Path, default="/etc/systemd/network")
+    parser.add_argument("-f", "--force", help="Force overwriting existing config", action=argparse.BooleanOptionalAction)
+    subparsers = parser.add_subparsers(title="command", required=True)
+
+    parser_init = subparsers.add_parser('init')
+    parser_init.add_argument("-i", "--interface", help="Wireguard interface to create", default="wg0")
+    parser_init.add_argument("-p", "--port", help="Wireguard interface listen port", type=int, default=51820)
+    parser_init.add_argument("-n", "--network", help="IP network CIDR", type=ipaddress.ip_network, default="172.17.2.0/24")
+    parser_init.set_defaults(func=init_network)
+
     args = parser.parse_args()
-    cmd = args.command[0]
 
     try:
-        commands[cmd]()
+        args.func(vars(args))
         return 0
     except Exception as e:
-        print("Error: ", e)
+        print("Error: ", e, file=sys.stderr)
         return 1
 
 if __name__ == "__main__":
---
+++ wg.py
@@ -0,0 +1,28 @@
+import codecs
+import secrets
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+
+"""wg is a python implementation of a subset of the utilities provided by the
+"wg" command from the "wireguard-tools" package.
+"""
+
+def genkey() -> str:
+        """Generates a random private key in base64 and returns it"""
+        key = X25519PrivateKey.generate()
+
+        bytes = key.private_bytes_raw( )
+
+        return codecs.encode(bytes, 'base64').decode('utf-8').strip()
+
+def pubkey(private_key: str) -> str:
+        """Calculates a public key and returns it in base64 from a corresponding private key (generated with genkey) given in base64"""
+        bytes_raw = codecs.encode(private_key, 'utf-8').decode("base64")
+        key = X25519PrivateKey.from_private_bytes(bytes_raw)
+
+        pubkey_bytes = key.public_key().public_bytes_raw()
+
+        return codecs.encode(pubkey_bytes, 'base64').decode('utf-8').strip()
+
+def genpsk() -> str:
+        """genpsk Generates a random preshared key in base64 and returns it"""
+        return codecs.encode(secrets.token_bytes(32), "base64").decode("utf-8").strip()
